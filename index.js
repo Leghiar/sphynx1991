c390b8a6dbb9b1c34394c959f3bec74cc9349750
Leghiar <39971250+Leghiar@users.noreply.github.com>
2018-06-07 19:35:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+var prefix = "$"
+
+var colors = require('colors');
+
+var colors = require('colors/safe');
+
+bot.login('NDUzNjU0MzczMjE0NTg0ODMy.DfiRrQ.1zRpBlxpJWuF6m7O-Lb3Sv3aVIw')
+
+bot.on('ready', function ()  {
+  bot.user.setGame("Command: $help");
+  console.log('Connected'.green);
+});
+
+bot.on('message', msg => {
+  if (msg.content === prefix + 'help') {
+    msg.channel.sendMessage('__Liste des commandes :__ \n \n $tt \n $fabriquant \n $cri \n $deut \n $met');
+                                 }
+	                    });
+
+bot.on('message', msg => {
+  if (msg.content === prefix + 'tt') {
+  msg.channel.sendMessage('Création du bot le __07/06/2018__');
+  console.log('Commande effectuée'.cyan);
+                                       }
+                     });
+
+bot.on('message', msg => {
+ if (msg.content === prefix + 'fabriquant') {
+ msg.reply('__Ce bot a été créé par Leghiar (Karnak)__');
+ console.log('Commande effectuée'.america);
+                                                            }
+                                          });
+
+
+bot.on('message', msg => {
+  if (msg.content === prefix + 'deut') {
+    msg.channel.sendMessage('@everyone **deut à vendre ! :)**');
+                                 }
+	                    });
+
+bot.on('message', msg => {
+  if (msg.content === prefix + 'cri') {
+    msg.channel.sendMessage('@everyone **cristal à vendre ! :)**');
+                                 }
+                            });
+
+
+bot.on('message', msg => {
+  if (msg.content === prefix + 'met') {
+    msg.channel.sendMessage('@everyone **métal à vendre ! :)**');
+                                 }
+                            });
+
+bot.on('message', msg => {
+  if (msg.content === '$$vendu') {
+    msg.channel.sendMessage('**Adjugé vendu !**');
+                                 }
+                            });
+
+bot.on('message', msg => {
+  if (msg.content === 'Quizz du Sphynx') {
+    msg.channel.sendMessage('Qu-est ce qui a 4 pattes le matin, 2 l après-midi et 3 le soir ?')     }
+                            });
